b439a8913d11f01589003acf10f17759b3c3bb86
Esohnobhn <ifadafavour50@gmail.com>
2026-08-31 13:47:08 +0100

    test: isolate vulnerable dependencies from source findings

--- fixture-a.js
+++
@@ -1,3 +0,0 @@
-export function parseUnsafe(value) {
-  return eval(value);
-}
--- fixture-b.py
+++
@@ -1,7 +0,0 @@
-# harmless comment added during Revision B
-# another harmless comment added during Revision B
-
-import requests
-
-def fetch_unsafe(url):
-    return requests.get(url, verify=False)
